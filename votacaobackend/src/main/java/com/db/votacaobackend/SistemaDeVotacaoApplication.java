8154aef2428cb8a6f7dd850ec6c7a77a99f203ff
nataliaschmidt <nataliak.schmidt@gmail.com>
2025-04-18 15:59:43 -0300

    chore: initial project setup

---
+++ votacaobackend/src/main/java/com/db/votacaobackend/SistemaDeVotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.db.votacaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVotacaoApplication.class, args);
+	}
+
+}
