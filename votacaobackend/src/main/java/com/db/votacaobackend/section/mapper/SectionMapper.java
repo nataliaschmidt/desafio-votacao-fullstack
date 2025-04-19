171b0d50b3b53e01f12fa6a47d7003732c1dcf23
nataliaschmidt <nataliak.schmidt@gmail.com>
2025-04-19 12:25:09 -0300

    chore: remove unused method

--- votacaobackend/src/main/java/com/db/votacaobackend/section/mapper/SectionMapper.java
+++ votacaobackend/src/main/java/com/db/votacaobackend/section/mapper/SectionMapper.java
@@ -1,6 +1,5 @@
 package com.db.votacaobackend.section.mapper;
 
-import com.db.votacaobackend.section.dto.CreateSectionDTO;
 import com.db.votacaobackend.section.dto.SectionDetailsDTO;
 import com.db.votacaobackend.section.model.Section;
 import org.springframework.stereotype.Component;
@@ -8,11 +7,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class SectionMapper {
 
-  public Section toEntity(CreateSectionDTO createSectionDTO) {
-    return Section.builder()
-        .duration(createSectionDTO.duration())
-        .build();
-  }
 
   public SectionDetailsDTO toDto(Section section) {
     return new SectionDetailsDTO(
