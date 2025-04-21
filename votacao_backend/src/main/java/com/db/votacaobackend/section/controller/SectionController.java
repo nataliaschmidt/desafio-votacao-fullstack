45153f9feeea251954a4a6eeafaa131ff5216f3e
nataliaschmidt <nataliak.schmidt@gmail.com>
2025-04-21 09:32:27 -0300

    docs: (backend) creates documentation in Swagger

--- votacao_backend/src/main/java/com/db/votacaobackend/section/controller/SectionController.java
+++ votacao_backend/src/main/java/com/db/votacaobackend/section/controller/SectionController.java
@@ -7,10 +7,16 @@ import com.db.votacaobackend.section.dto.CreateSectionDTO;
 import com.db.votacaobackend.section.dto.SectionDetailsDTO;
 import com.db.votacaobackend.section.service.SectionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,8 +33,23 @@ public class SectionController {
 
   @PostMapping
   @Operation(
-      summary = "Create section"
+      summary = "Criação de uma sessão",
+      description = "Cria uma sessão para uma pauta existente. Somente é permitido criar sessões para pautas que ainda não possuem sessão vinculada."
   )
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Sessão salva com sucesso"),
+      @ApiResponse(
+          responseCode = "404",
+          description = "Pauta não encontrada para realizar a criação da sessão",
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class),
+              examples = @ExampleObject(
+                  value = "{ \"message\": \"Pauta não encontrada\" }"
+              )
+          )
+      )
+  })
   public ResponseEntity<SectionDetailsDTO> createSection(@RequestBody CreateSectionDTO section) {
     SectionDetailsDTO savedSection = service.createSection(section);
     return ResponseEntity.status(HttpStatus.CREATED).body(savedSection);
@@ -36,7 +57,8 @@ public class SectionController {
 
   @GetMapping("/open")
   @Operation(
-      summary = "List open sections"
+      summary = "Listagem das sessões que estão abertas",
+      description = "Listas as sessões que ainda estão com o tempo final posterior ao momento atual"
   )
   public ResponseEntity<List<SectionDetailsDTO>> listOpenSections() {
     List<SectionDetailsDTO> openSections = service.listOpenSections();
@@ -45,14 +67,19 @@ public class SectionController {
 
   @GetMapping
   @Operation(
-      summary = "List all sections"
+      summary = "Listagem de todas as sessões"
   )
   public ResponseEntity<List<SectionDetailsDTO>> listAllSessions() {
     List<SectionDetailsDTO> openSections = service.listAllSessions();
     return ResponseEntity.status(HttpStatus.OK).body(openSections);
   }
+
+  @Operation(
+      summary = "Listagem das pautas disponíveis",
+      description = "Listas as pautas disponíveis para que possa ser criada uma sessão"
+  )
   @GetMapping("/availiable-agendas")
-  public ResponseEntity<List<AgendaDTO>> listAllAvailiableAgendas(){
+  public ResponseEntity<List<AgendaDTO>> listAllAvailiableAgendas() {
     List<Agenda> availiableAgendas = service.listAllAvailiableAgendas();
     List<AgendaDTO> availiableAgendasDto = availiableAgendas.stream()
         .map(agendaMapper::toDto)
--- votacao_backend/src/main/java/com/db/votacaobackend/vote/controller/VoteController.java
+++ votacao_backend/src/main/java/com/db/votacaobackend/vote/controller/VoteController.java
@@ -5,10 +5,16 @@ import com.db.votacaobackend.vote.dto.VoteDetailDTO;
 import com.db.votacaobackend.vote.dto.VoteResultsDTO;
 import com.db.votacaobackend.vote.service.VoteService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,8 +31,46 @@ public class VoteController {
 
   @PostMapping
   @Operation(
-      summary = "Create vote"
+      summary = "Criação de um voto",
+      description = "Você poderá votar em uma sessão que esteja aberta."
+          + "O cpf só pode votar uma vez na mesma sessão"
   )
+  @ApiResponses({
+      @ApiResponse(responseCode = "201", description = "Voto realizado com sucesso"),
+      @ApiResponse(
+          responseCode = "400",
+          description = "Sessão já encerrada",
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class),
+              examples = @ExampleObject(
+                  value = "{ \"message\": \"Sessão já está encerrada!\" }"
+              )
+          )
+      ),
+      @ApiResponse(
+          responseCode = "400",
+          description = "Sessão já encerrada",
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class),
+              examples = @ExampleObject(
+                  value = "{ \"message\": \"Sessão já está encerrada!\" }"
+              )
+          )
+      ),
+      @ApiResponse(
+          responseCode = "409",
+          description = "Cpf já votou nessa sessão",
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class),
+              examples = @ExampleObject(
+                  value = "{ \"message\": \"Este CPF já votou!\" }"
+              )
+          )
+      )
+  })
   public ResponseEntity<VoteDetailDTO> createVote(@RequestBody VoteCreateDTO vote) {
     VoteDetailDTO createdVote = service.createVote(vote);
     return ResponseEntity.status(HttpStatus.CREATED).body(createdVote);
@@ -34,14 +78,33 @@ public class VoteController {
 
   @GetMapping
   @Operation(
-      summary = "List all votes"
+      summary = "Listagem de todos os votos"
   )
   public ResponseEntity<List<VoteDetailDTO>> listAllVotes() {
     List<VoteDetailDTO> openSections = service.listAllVotes();
     return ResponseEntity.status(HttpStatus.OK).body(openSections);
   }
 
+
   @GetMapping("/results/{sectionId}")
+  @Operation(
+      summary = "Contabiliza os votos por sessão",
+      description = "Contabiliza os votos através do id da sessão"
+  )
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Contabilização dos votos"),
+      @ApiResponse(
+          responseCode = "404",
+          description = "Sessão não encontrada para contabilizar seus votos",
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(implementation = ErrorResponse.class),
+              examples = @ExampleObject(
+                  value = "{ \"message\": \"Sessão não encontrada!\" }"
+              )
+          )
+      )
+  })
   public ResponseEntity<VoteResultsDTO> getVoteResults(@PathVariable("sectionId") Long sectionId) {
     VoteResultsDTO resultsDTO = service.listVotesFromSession(sectionId);
     return ResponseEntity.status(HttpStatus.OK).body(resultsDTO);
